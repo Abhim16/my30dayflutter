9baec197be36375bea839f7bc5f13b8db5c51d2c
Abhim16 <abhishekmishra1662000@gmail.com>
2022-02-28 10:39:23 +0530

    day10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item{
+final String id;
+final String name;
+final String desc;
+final num price;
+final String color;
+final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+final product =[
+  Item(id:"ama1",
+   name:"iphone 12",
+    desc:"purple with matt finish",
+     price:350,
+      color:"#33505a",
+       image:"https://cdn.pocket-lint.com/r/s/1200x/assets/images/146898-phones-feature-the-best-apple-iphone-photos-ever-taken-image1-ydter7skel.jpg";
+  )
+    
+];
